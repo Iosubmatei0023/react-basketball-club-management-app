6ad9568b41e4dfcacdf38e8699e02f68ad3c3992
Iosubmatei0023 <iosubmatei0023@gmail.com>
2025-07-14 12:54:28 +0300

    cleaned code

--- src/components/Auth.js
+++
@@ -1,9 +0,0 @@
-// export const Auth = () => {
-//   return (
-//     <div>
-//       <input placeholder="Email..." />
-//       <input placeholder="Password..." />
-//       <button>Sign In</button>
-//     </div>
-//   );
-// };
